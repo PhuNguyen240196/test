0453a5615b6415d194fedd44f2a537e947dac364
PhuNguyen240196 <38210727+PhuNguyen240196@users.noreply.github.com>
2019-06-25 16:19:44 +0700

    first commit

---
+++ src/main/java/com/javainuse/controller/WebSocketChatController.java
@@ -0,0 +1,31 @@
+package com.javainuse.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.javainuse.domain.WebSocketChatMessage;
+
+import reactor.ipc.netty.http.server.HttpServerRequest;
+
+@Controller
+public class WebSocketChatController {
+
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/javainuse")
+	public WebSocketChatMessage sendMessage(@Payload WebSocketChatMessage webSocketChatMessage) {
+		return webSocketChatMessage;
+	}
+
+	@MessageMapping("/chat.newUser")
+	@SendTo("/topic/javainuse")
+	public WebSocketChatMessage addUser(@Payload WebSocketChatMessage webSocketChatMessage,
+			SimpMessageHeaderAccessor headerAccessor) {
+		
+		headerAccessor.getSessionAttributes().put("username", webSocketChatMessage.getSender());
+		return webSocketChatMessage;
+	}
+
+}
